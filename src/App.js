41bfdab3aa0f7f332c7883e797b3ba94d211650c
Aditya S Sooraj <88623913+PhilRyan88@users.noreply.github.com>
2025-01-06 22:25:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     e.preventDefault();
   };
   return (
-    <div className="App bg-blue-900 h-screen w-screen relative overflow-hidden flex justify-center items-center">
+   
       <div className="h-40-r w-40-r bg-gradient-to-r from-yellow-400 to-blue-500 rounded-full absolute left-2/3 -top-56 transform rotate-160 animate-pulse"></div>
       <div className="h-35-r w-35-r bg-gradient-to-r from-pink-400 via-pink-500 to-red-500 rounded-full absolute top-96 -left-20 transform rotate-180 animate-pulse"></div>
       <Tilt>
